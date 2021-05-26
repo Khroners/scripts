28320a83fca48c1dbc6a78f12e66d3342675e08e
Khroners <75507405+Khroners@users.noreply.github.com>
2021-05-26 02:26:24 +0200

    Update debian_postinstall_traefik_portainer.sh

--- debian_postinstall_traefik_portainer.sh
+++ debian_postinstall_traefik_portainer.sh
@@ -49,7 +49,7 @@ function Install-Docker {
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt update
-  apt install docker-ce docker-ce-cli containerd.io
+  apt install -y docker-ce docker-ce-cli containerd.io
 }
 
 
